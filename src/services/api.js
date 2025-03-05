6506696b8a6958f70141c414e048500c3445a4e5
Sharon N Nabiryo <sharongless37@gmail.com>
2025-03-04 21:17:33 -0500

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "2fd668329a7fdf69661495a6026b4a25";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
